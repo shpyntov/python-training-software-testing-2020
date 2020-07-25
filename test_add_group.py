880ab373f44bd1d93038249d9f9de4880ca96c76
Yury Shpyntov <Yuriy.Shpyntov@rt.ru>
2020-07-25 22:22:01 +0300

    add methods and class

--- test_add_group.py
+++ test_add_group.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 import unittest
+from group import Group
 
 
 class TestAddGroup(unittest.TestCase):
@@ -10,29 +11,55 @@ class TestAddGroup(unittest.TestCase):
 
     def test_add_group(self):
         driver = self.driver
-        driver.get("https://localhost/")
+        self.open_homepage(driver)
+        self.login(driver, username="admin", password="secret")
+        self.open_groups_page(driver)
+        self.create_group(driver, Group(name="test group", header="test 12345 _", footer="test 12345 +"))
+        self.go_to_groups_page(driver)
+        self.logout(driver)
+
+    def test_add_empty_group(self):
+        driver = self.driver
+        self.open_homepage(driver)
+        self.login(driver, username="admin", password="secret")
+        self.open_groups_page(driver)
+        self.create_group(driver, Group(name="", header="", footer=""))
+        self.go_to_groups_page(driver)
+        self.logout(driver)
+
+    def login(self, driver, username, password):
         driver.find_element_by_name("user").click()
         driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("user").send_keys(username)
         driver.find_element_by_name("pass").click()
         driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_name("pass").send_keys(password)
         driver.find_element_by_xpath("//input[@value='Login']").click()
-        driver.find_element_by_link_text("groups").click()
+
+    def open_homepage(self, driver):
+        driver.get("https://localhost/")
+
+    def logout(self, driver):
+        driver.find_element_by_link_text("Logout").click()
+
+    def go_to_groups_page(self, driver):
+        driver.find_element_by_link_text("group page").click()
+
+    def create_group(self, driver, group):
         driver.find_element_by_name("new").click()
         driver.find_element_by_name("group_name").click()
         driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys("test group")
+        driver.find_element_by_name("group_name").send_keys(group.name)
         driver.find_element_by_name("group_header").click()
         driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys("test 12345 _")
+        driver.find_element_by_name("group_header").send_keys(group.header)
         driver.find_element_by_name("group_footer").click()
         driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys("test 12345 +")
+        driver.find_element_by_name("group_footer").send_keys(group.footer)
         driver.find_element_by_name("submit").click()
-        driver.find_element_by_link_text("group page").click()
-        driver.find_element_by_link_text("Logout").click()
-        driver.find_element_by_id("content").click()
+
+    def open_groups_page(self, driver):
+        driver.find_element_by_link_text("groups").click()
 
     def tearDown(self):
         self.driver.quit()
